d5c1d91bbdb358a093e2bd17da543d7e5718386c
maryedesigns <mary.erhieyovwe4christ@gmail.com>
2021-08-04 11:22:47 +0100

    changes

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const userRoute = require('./Route/userRoute')
 
 const app = express();
 
-
+const ngrok = require('ngrok');
+(async function() {
+  const url = await ngrok.connect(3000);
+  console.log(url);
+})();
 
 app.use('/users',userRoute)
 
